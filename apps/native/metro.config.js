9bb18533a0ac4d12d595243f2288ad81d91f9dfd
Lucas <lucaspraseldev@gmail.com>
2026-03-06 02:07:19 -0300

    notworking

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,9 +1,29 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withUniwindConfig } = require("uniwind/metro");
-const { wrapWithReanimatedMetroConfig } = require("react-native-reanimated/metro-config");
+const {
+  wrapWithReanimatedMetroConfig,
+} = require("react-native-reanimated/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+const SERVER_ONLY_PACKAGES = [
+  "@XoGordura/db",
+  "@XoGordura/auth",
+  "@neondatabase/serverless",
+  "better-auth",
+  "drizzle-orm",
+];
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  const isServerOnly = SERVER_ONLY_PACKAGES.some(
+    (pkg) => moduleName === pkg || moduleName.startsWith(pkg + "/"),
+  );
+  if (isServerOnly) {
+    return { type: "empty" };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 const uniwindConfig = withUniwindConfig(wrapWithReanimatedMetroConfig(config), {
   cssEntryFile: "./global.css",
